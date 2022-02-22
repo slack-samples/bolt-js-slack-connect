a1b5411e8b7d11f71b97d40aba76fcc740ee6b78
Horea Porutiu <hporutiu@slack-corp.com>
2022-02-22 15:06:20 -0800

    Revert "adding sample"
    This reverts commit 639263c62d3b0e8ec9d232f654d79dea0274ec5c.

--- listeners/index.js
+++
@@ -1,9 +0,0 @@
-const actions = require('./actions');
-const events = require('./events');
-const views = require('./views');
-
-module.exports.registerListeners = (app) => {
-  actions.register(app);
-  events.register(app);
-  views.register(app);
-};
--- templates/index.js
+++
@@ -1,4 +0,0 @@
-const orgAdminInstall = require('./org_admin_install');
-const workspaceInstall = require('./workspace_install');
-
-module.exports = {orgAdminInstall, workspaceInstall};
