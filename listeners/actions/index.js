09140ff3501d64f78ecc99bd48a1b00f5e9a9596
Horea Porutiu <hporutiu@slack-corp.com>
2022-05-20 17:12:13 -0700

    fixed linting errors

--- listeners/actions/index.js
+++ listeners/actions/index.js
@@ -1,5 +1,4 @@
 const { listInvitesAction } = require('./list_invites');
-const { acceptInvite } = require('./accept_invite');
 const { submitSharedChannelInvite } = require('./submit_channel_invite');
 const { disconnectChannel } = require('./disconnect_channel');
 const { approveInvite } = require('./approve_invite');
@@ -8,7 +7,6 @@ const { denyInvite } = require('./deny_invite');
 module.exports.register = (app) => {
   app.action('submit_invite_action', submitSharedChannelInvite);
   app.action('list_invites_action', listInvitesAction);
-  app.action('accept_action', acceptInvite);
   app.action('disconnect_channel', disconnectChannel);
   app.action('approve_action', approveInvite);
   app.action('deny_action', denyInvite);
